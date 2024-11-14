e6d9a94bb03000869ec16611647eb969c27820d3
Hugo Valle <hugovalle1@weber.edu>
2024-11-14 19:29:17 +0000

    Update pep 8 formatting

--- module11/airtravel.py
+++ module11/airtravel.py
@@ -12,10 +12,10 @@ class Flight:
         self._aircraft = aircraft
         # Get rows and seats
         rows, seats = self._aircraft.seating_plan()
-        # 'Waste" index 0. 
-        self._seating = [None] + [{letter: None for letter in seats} 
+        # 'Waste" index 0.
+        self._seating = [None] + [{letter: None for letter in seats}
                                   for _ in rows]
-    
+
     def _validate_flight_number(self, number):
         # If any of the rules below fail, raise ValueError
         # Must be 5 characters long
@@ -31,10 +31,10 @@ class Flight:
         if not number[2:].isdigit():
             raise ValueError(f'Invalid flight number [{number[2:]}]')
         return number
-    
+
     # Functions inside classes are called methods
     # This is also called a "getter" method
-    # First parameter MUST be 'self' is a reference to 
+    # First parameter MUST be 'self' is a reference to
     # the object that is being created
     def number(self):
         return self._number
@@ -42,27 +42,27 @@ class Flight:
     # This is also called a "setter" method
     def set_number(self, number):
         self._number = self._validate_flight_number(number)
-    
+
     def aircraft_model(self):
         return self._aircraft.model()
-    
+
     def _parse_seat(self, seat):
         rows, seat_letters = self._aircraft.seating_plan()
         # Validate seat letter. Ex '2B'
         letter = seat[-1]  # take last char
         if letter not in seat_letters:
-            raise ValueError(f'Invalid seat letter {letter}')   
+            raise ValueError(f'Invalid seat letter {letter}')
         # Validate the row number
-        row_text = seat[:-1] # take all but last char
+        row_text = seat[:-1]  # take all but last char
         try:
             row = int(row_text)  # str to int conversion
         except:
-            raise ValueError(f'Invalid seat row {row_text}')   
+            raise ValueError(f'Invalid seat row {row_text}')
         if row not in rows:  # valid number?
-            raise ValueError(f'Invalid seat row {row_text}')   
-        
+            raise ValueError(f'Invalid seat row {row_text}')
+
         return row, letter
-    
+
     def allocate_seat(self, seat, passenger):
         row, letter = self._parse_seat(seat)
         # check if seat is occupied
@@ -70,9 +70,6 @@ class Flight:
             raise ValueError(f'Seat {seat} already occupied')
         # Otherwise is good to go
         self._seating[row][letter] = passenger
-        
-        
-            
 
 
 class Aircraft:
@@ -81,37 +78,36 @@ class Aircraft:
         self._model = model
         self._num_rows = num_rows
         self._num_seats_per_row = num_seats_per_row
-        
+
     # Add setters and getters for all instance variables
 
     def registration(self):
         return self._registration
-    
+
     def set_registration(self, registration):
         self._registration = registration
-    
+
     def model(self):
         return self._model
-    
+
     def set_model(self, model):
         self._model = model
-    
+
     def num_rows(self):
         return self._num_rows
-    
+
     def set_num_rows(self, num_rows):
         self._num_rows = num_rows
-        
+
     def num_seats_per_row(self):
         return self._num_seats_per_row
-        
+
     def set_num_seats_per_row(self, num_seats_per_row):
         self._num_seats_per_row = num_seats_per_row
-            
+
     def seating_plan(self):
         return (range(1, self._num_rows + 1),
                 'ABCDEFGHJK'[:self._num_seats_per_row])
-            
 
 
 def main():
