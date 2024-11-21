4aa13f021e23929c555884b1b59798e77bec5a35
Hugo Valle <hugovalle1@weber.edu>
2024-11-21 18:11:46 +0000

    add __eq__() support

--- module12/person.py
+++ module12/person.py
@@ -21,6 +21,22 @@ class Person:
         """
         return f"Person's info, name[{self._name}], age[{self._age}]"
 
+    def __eq__(self, other):
+        """Comparison Operator
+
+        Args:
+            other (Person): Object to compare to
+
+        Raises:
+            TypeError: Incorrect Type
+
+        Returns:
+            Boolean: True if objects have same age, False otherwise
+        """
+        if not isinstance(other, Person):  # check if other is type Person
+            raise TypeError('Unsupported operand type == for these objects')
+        return self._age == other._age  # True or False
+
 
 def main():
     pass
--- module12/use_person.py
+++ module12/use_person.py
@@ -6,8 +6,10 @@ def main():
     print('Testing Person class')
     p1 = Person('Waldo Weber', 18)
     print(type(p1), p1)
-    p2 = Person('Superman', 120)
+    p2 = Person('Superman', 18)
     print(type(p2), p2)
+    if p1 == p2:
+        print('You are the same age')
 
 
 if __name__ == '__main__':
