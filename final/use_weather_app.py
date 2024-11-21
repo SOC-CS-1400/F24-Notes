34f046f04a11419450ca5217d86100dbb1d651c2
Hugo Valle <hugovalle1@weber.edu>
2024-11-21 18:43:20 +0000

    Initial commit

---
+++ final/use_weather_app.py
@@ -0,0 +1,14 @@
+
+"""Test the Weather App
+"""
+import weather_app as wa
+
+
+def main():
+    print('Testing Weather App')
+    print(wa.BASE_URL)
+
+
+if __name__ == '__main__':
+    # Call main function
+    main()
---
+++ final/weather_app.py
@@ -0,0 +1,21 @@
+
+"""Weather App to check current weather conditions"""
+# install package, type on your terminal:
+# pip install requests
+import requests 
+
+API_KEY = 'af4f939501ebb649537e015196aedc70' # YOUR OWN KEY
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?'
+
+
+def pull_weather_data(zip_code):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    # Call main function
+    main()
